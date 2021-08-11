1e32c8a31f63c17ba0039ff43d602df5c4287ee0
Rodrigo Lima <limarodrigoo@outlook.com>
2021-08-11 02:57:01 -0300

    Recebendo input de Sensibilidade

--- script.js
+++ script.js
@@ -11,4 +11,31 @@ function createOptionGames(selector) {
 createOptionGames('gameIn');
 createOptionGames('gameOut');
 
+let gameSensIn;
+let gameSensOut;
 
+function getGameSensIn() {
+  gameSensIn = parseFloat(document.getElementById('sensIn').value);
+  gameSensOut = convertingSens(gameSensIn);
+  document.getElementById('sensOut').value = gameSensOut;
+  console.log(gameSensIn);
+  return gameSensIn;
+}
+
+function convertingSens(gameSensIn){
+  let inputGame = document.getElementById('gameIn').value;
+  let outputGame = document.getElementById('gameOut').value;
+
+  switch (inputGame) {
+    case 'CS:GO':
+      gameSensOut = gameSensIn + 1;
+      break;
+    case 'Fortnite':
+      gameSensOut = gameSensIn / 10;
+      break;
+    default:
+      gameSensOut = gameSensIn + 10;    
+  }
+
+  return gameSensOut;
+}
