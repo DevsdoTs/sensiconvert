50ae88b6bbac126a39e7ab7870d9b504755e1007
Rodrigo Lima <limarodrigoo@outlook.com>
2021-08-11 02:10:14 -0300

    Criando HTML e Options de jogos pelo JS

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-const headerElement = document.createElement('header');
-document.body.appendChild(headerElement);
+const games = ['CS:GO', 'Fortnite', 'Apex Legends', 'Battlefield V', 'Overwatch', 'PUBG', 'Valorant'];
 
-const titleHeader = document.createElement('h1');
-titleHeader.innerHTML = 'Sens-Converter';
-document.querySelector('header').appendChild(titleHeader);
+function createOptionGames(selector) {
+  for (let index in games) {
+    let gamesSelect = document.createElement('option');
+    gamesSelect.innerHTML = games[index];
+    document.getElementById(selector).appendChild(gamesSelect);
+  }
+}
 
-const titleComment = document.createElement('h2');
-titleComment.innerHTML = 'Converta sua sensibilidade';
-document.querySelector('header').appendChild(titleComment);
+createOptionGames('gameIn');
+createOptionGames('gameOut');
 
 
-const mainElement = document.createElement('main');
-document.body.appendChild(mainElement);
-
-const formElement = document.createElement('form');
-document.querySelector('main').appendChild(formElement);
-
-let divElement = document.createElement('div');
-divElement.id = 'inputSens'
-document.querySelector('form').appendChild(divElement);
-
-let labelElement = document.createElement('label');
-labelElement.setAttribute('for =','sens');
-labelElement.innerHTML = 'Sensibilidade';
-document.getElementById('inputSens').appendChild(labelElement);
-
-let inputElement = document.createElement('input');
-inputElement.setAttribute('type', 'sens');
-inputElement.setAttribute('name', 'senibilidade');
-document.getElementById('inputSens').appendChild(inputElement);
