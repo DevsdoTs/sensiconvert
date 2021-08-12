76a61b6ff8829e4e41cca3c9b38b5baefec3fb11
Rodrigo Lima <limarodrigoo@outlook.com>
2021-08-11 23:41:46 -0300

    Add function to convert sens

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const games = ['CS:GO', 'Fortnite', 'Apex Legends', 'Battlefield V', 'Overwatch', 'PUBG', 'Valorant'];
+const games = ['CS:GO', 'Fortnite', 'Apex Legends','Battlefield V', 'Overwatch', 'Valorant'];
 
 function createOptionGames(selector) {
   for (let index in games) {
@@ -11,31 +11,87 @@ function createOptionGames(selector) {
 createOptionGames('gameIn');
 createOptionGames('gameOut');
 
-let gameSensIn;
-let gameSensOut;
 
-function getGameSensIn() {
-  gameSensIn = parseFloat(document.getElementById('sensIn').value);
-  gameSensOut = convertingSens(gameSensIn);
-  document.getElementById('sensOut').value = gameSensOut;
-  console.log(gameSensIn);
-  return gameSensIn;
+function converting() {
+  const sensIn = document.getElementById('sensIn');
+  const gameIn = document.getElementById('gameIn');
+  const gameOut = document.getElementById('gameOut');
+  let gameSensOut = document.getElementById('sensOut').value;
+
+  gameSensOut = sensIn.addEventListener('change', function () {
+    let sensInValue = parseFloat(sensIn.value);
+    let gameInValue = gameIn.value;
+    let gameOutValue = gameOut.value;
+    
+    let sensCs = convertingToCs(gameInValue, sensInValue);
+    return convertingFromCs(gameOutValue, sensCs);
+
+  })
+
 }
 
-function convertingSens(gameSensIn){
-  let inputGame = document.getElementById('gameIn').value;
-  let outputGame = document.getElementById('gameOut').value;
+function convertingToCs(gameInValue, sensInValue) {
+  let sensCS = gamesIn[gameInValue].base + ((sensInValue - 1) * gamesIn[gameInValue].offset);
+  return sensCS;
+}
 
-  switch (inputGame) {
-    case 'CS:GO':
-      gameSensOut = gameSensIn + 1;
-      break;
-    case 'Fortnite':
-      gameSensOut = gameSensIn / 10;
-      break;
-    default:
-      gameSensOut = gameSensIn + 10;    
+function convertingFromCs(gameOutValue, sensCS){
+  let sensOut = gamesOut[gameOutValue].base + ((sensCS - 1) * gamesOut[gameOutValue].offset);
+  return sensOut;
+}
+
+const gamesIn = {
+  'CSGO': {
+    base: 0,
+    offset: 1
+  },
+  'Apex Legends': {
+    base: 0,
+    offset: 1
+  },
+  'Fortnite': {
+    base: 0.2525,
+    offset: 0.2525
+  },
+  'Valorant': {
+    base: 3.181818,
+    offset: 3.181818
+  },
+  'Overwatch': {
+    base: 0.841667,
+    offset: 0.841667
+  },
+  'Battlefield V': {
+    base: 0.677132,
+    offset: 0.156261
   }
-
-  return gameSensOut;
 }
+
+
+const gamesOut = {
+  'CSGO': {
+    base: 0,
+    offset: 1
+  },
+  'Apex Legends': {
+    base: 0,
+    offset: 1
+  },
+  'Fortnite': {
+    base: 3.960396,
+    offset: 3.960396
+  },
+  'Valorant': {
+    base: 0.314286,
+    offset: 0.314286
+  },
+  'Overwatch': {
+    base: 3.33333,
+    offset: 3.33333
+  },
+  'Battlefield V': {
+    base: 3.066207,
+    offset: 6.399541
+  }
+}
+
